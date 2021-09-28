93efb87f47cee9216abadab57a3819cdaaa41082
Jan Morales <moralesjan40@gmail.com>
2021-09-28 14:46:43 +0000

    Completed all problems

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 //1
 function countToFifty(){
+  for(let i = 1; i < 51; i++ ){
+      console.log(i)
+  }
   
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+    
+  for(let i = 1; i < num; i++ ){
+      console.log(i)
+  }
 }
 
 //3
 function threeAndFiveInFifty(){
-  
+
+    for(let i =1; i < 51; i++){
+        if (i % 3 === 0 || i % 5 === 0){
+            console.log(i)
+        }
+    }
 }
 
 //4
-function threeAndFive(){
-  
+function threeAndFive(num){
+   for(let i =1; i < num; i++){
+        if (i % 3 === 0 || i % 5 === 0){
+            console.log(i)}
+}
 }
 
-
+countToFifty();
+countFromOne(9);
+threeAndFiveInFifty();
+threeAndFive(19);
